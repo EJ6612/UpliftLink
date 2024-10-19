781f956b88a3bad5b469058f4e6058d58ee3b730
Elijah Dodge <ej_6612@outlook.com>
2024-10-18 20:54:32 -0600

    feat: classes created

--- UpliftLink/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/UpliftLink.AssemblyInfo.cs
+++ UpliftLink/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/UpliftLink.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+27bde3cf3828388033fa15cf51c55aae796394a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2a3865e4129e172f734b261d90aebe68b3380d3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
