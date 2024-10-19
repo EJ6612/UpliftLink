7b412a4e9e3e280e7fc6008f83219fe771f821fb
Elijah Dodge <ej_6612@outlook.com>
2024-10-18 21:14:00 -0600

    test

--- UpliftLink/obj/Debug/net8.0-ios/iossimulator-x64/UpliftLink.AssemblyInfo.cs
+++ UpliftLink/obj/Debug/net8.0-ios/iossimulator-x64/UpliftLink.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2a3865e4129e172f734b261d90aebe68b3380d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d03d0bfbfe64651b6590b8c1e2807e8761667e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
