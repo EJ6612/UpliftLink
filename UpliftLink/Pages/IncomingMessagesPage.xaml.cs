455895d7b53d855c8168505f634e9ced5724b175
ej6612 <ej_6612@outlook.com>
2024-10-19 05:46:21 -0600

    Feat: Incoming/Outgoing

--- UpliftLink/Pages/IncomingMessagesPage.xaml.cs
+++ UpliftLink/Pages/IncomingMessagesPage.xaml.cs
@@ -5,6 +5,55 @@ public partial class IncomingMessagesPage : ContentPage
 	public IncomingMessagesPage()
 	{
 		InitializeComponent();
+
+		// foreach message in messages, maximum 20
+		CreateChatBubble("All the single ladies! All the single ladies! ", "Beyonce-With-A-Knife-3000");
+		CreateChatBubble("Are we out of the woods yet are we out of the woods yet are we out of the woods are we in the clear yet are we in the clear yet are in in the clear yet in the clear yet good", "Obsessive-Catlady-1989");
+	}
+
+	private void CreateChatBubble(string message, string senderName)
+	{
+		var messageLabel = new Label 
+		{
+			Text = message,
+			TextColor = Colors.White,
+			FontSize = 16, 
+			HorizontalOptions = LayoutOptions.Start,
+			VerticalOptions = LayoutOptions.Center
+
+		};
+
+		var chatBubble = new Frame 
+		{
+			Content = messageLabel,
+			Padding = new Thickness(15),
+			CornerRadius = 15,
+			HasShadow = true,
+			BorderColor = Colors.Transparent,
+			BackgroundColor = Colors.MediumBlue,
+			HorizontalOptions = LayoutOptions.Start,
+			Margin = new Thickness(1, 1),
+			WidthRequest = 250
+
+		};
+
+		var messageSenderName = new Label
+		{
+			Text = senderName,
+			FontSize = 14,
+			HorizontalOptions = LayoutOptions.Start
+		};
+
+		var chatObject = new VerticalStackLayout
+		{
+			Padding = new Thickness(0, 5, 0, 5),
+			Spacing = 5,
+		};
+
+		chatObject.Children.Add(messageSenderName);
+		chatObject.Children.Add(chatBubble);
+
+		StackLayout.Children.Add(chatObject);
 	}
 
 	
--- UpliftLink/Pages/OutgoingMessagesPage.xaml.cs
+++ UpliftLink/Pages/OutgoingMessagesPage.xaml.cs
@@ -5,5 +5,53 @@ public partial class OutgoingMessagesPage : ContentPage
 	public OutgoingMessagesPage()
 	{
 		InitializeComponent();
+		CreateChatBubble("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO AAAAAAAAAAAAAAAAAAAAAAAA", "Lift-me-up", 4);
+		CreateChatBubble("hehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehehe", "Quote", 2);
+		CreateChatBubble("oh yeah oh yeah i like that ooooohhhhhh my gooooooosh GOLLY GEE GOVNAR", "Service Idea", 249);
+	}
+
+	private void CreateChatBubble(string message, string category, int count)
+	{
+		var messageLabel = new Label 
+		{
+			Text = message,
+			TextColor = Colors.Black,
+			FontSize = 16, 
+			HorizontalOptions = LayoutOptions.Start,
+			VerticalOptions = LayoutOptions.Center
+
+		};
+
+		var chatBubble = new Frame 
+		{
+			Content = messageLabel,
+			Padding = new Thickness(15),
+			CornerRadius = 15,
+			HasShadow = true,
+			BorderColor = Colors.Transparent,
+			BackgroundColor = Colors.SkyBlue,
+			HorizontalOptions = LayoutOptions.End,
+			Margin = new Thickness(1, 1),
+			WidthRequest = 250
+
+		};
+
+		var messageSenderName = new Label
+		{
+			Text = $"{category}s sent: {count}",
+			FontSize = 14,
+			HorizontalOptions = LayoutOptions.End
+		};
+
+		var chatObject = new VerticalStackLayout
+		{
+			Padding = new Thickness(0, 5, 0, 5),
+			Spacing = 5,
+		};
+
+		chatObject.Children.Add(messageSenderName);
+		chatObject.Children.Add(chatBubble);
+
+		StackLayout.Children.Add(chatObject);
 	}
 }
