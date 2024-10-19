7a4b59f1b3715dd43ea8e560eb69cbcdf6b8da1c
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2024-10-19 02:28:21 -0600

    Update App xaml.cs

--- UpliftLink/App.xaml.cs
+++ UpliftLink/App.xaml.cs
@@ -1,18 +1,16 @@
-﻿using System.IO;
-using UpliftLink.Services;
+﻿using UpliftLink.Services;
 
 namespace UpliftLink
 {
     public partial class App : Application
     {
-        public static string UserPreferencesFilePath { get; private set; }
+        public static UserPreferencesService UserPreferencesService { get; private set; }
 
         public App()
         {
             InitializeComponent();
 
-            // Define the path for the user preferences JSON file
-            UserPreferencesFilePath = Path.Combine(FileSystem.AppDataDirectory, "user_preferences.json");
+            UserPreferencesService = new UserPreferencesService();
 
             MainPage = new AppShell();
         }
