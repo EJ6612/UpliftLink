5d1877dadc91963e1cde748cae28f0e8a7f4852f
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2024-10-18 22:22:32 -0600

    test. I updated user preferences

---
+++ UpliftLink/Services/ProximityService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpliftLink.Services
+{
+    internal class ProximityService
+    {
+        private bool isNearbyDevice;
+
+        public void StartProximityDetection()
+        {
+            // Logic to start scanning for nearby devices
+        }
+
+        public void StopProximityDetection()
+        {
+            // Logic to stop scanning for nearby devices
+        }
+    }
+}
--- UpliftLink/UserPref/UserPreferences.cs
+++ UpliftLink/UserPref/UserPreferences.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace UpliftLink.UserPref
 {
-    internal class UserPreferences
+    public class UserPreferences
     {
+        public bool IsVisible { get; set; }
+        public List<string> PersonalizedMessages { get; set; }
+
+        public UserPreferences()
+        {
+            PersonalizedMessages = new List<string>();
+        }
+
+        public void SavePreferences()
+        {
+            // Logic to save preferences (e.g., to local storage)
+        }
+
+        public void LoadPreferences()
+        {
+            // Logic to load preferences (e.g., from local storage)
+        }
     }
 }
