fd7a5f008b90aa70af59eae891a125ac67a08290
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2024-10-19 03:18:14 -0600

    Icons and info

--- UpliftLink/obj/Debug/net8.0-ios/iossimulator-x64/UpliftLink.AssemblyInfo.cs
+++ UpliftLink/obj/Debug/net8.0-ios/iossimulator-x64/UpliftLink.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+de5ab0b6127feaf6394d96373fa59465edd9221a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+717c8f322e6aba160a12253de087e3a4562df88b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
