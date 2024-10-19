367c3bc140e9d7633894146921d9f530ff0b0ffd
ej6612 <ej_6612@outlook.com>
2024-10-19 01:02:32 -0600

    Feat: SelectIncomingMessagePage
    User can select their preference of incoming message

--- UpliftLink/Pages/SelectIncomingMessagesPage.xaml.cs
+++ UpliftLink/Pages/SelectIncomingMessagesPage.xaml.cs
@@ -2,8 +2,21 @@ namespace UpliftLink.Pages;
 
 public partial class SelectIncomingMessagesPage : ContentPage
 {
+
+	private string userPreference = "";
 	public SelectIncomingMessagesPage()
 	{
 		InitializeComponent();
 	}
+
+    private void IncomingMessagesPreference_SelectedIndexChanged(object sender, EventArgs e)
+    {
+		var picker = (Picker)sender;
+		userPreference = (string)picker.SelectedItem;
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new IncomingMessagesPage());
+    }
 }
