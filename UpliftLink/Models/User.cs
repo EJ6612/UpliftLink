de5ab0b6127feaf6394d96373fa59465edd9221a
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2024-10-19 02:42:12 -0600

    JSON and User

---
+++ UpliftLink/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpliftLink.Models
+{
+    public class User
+    {
+        public string DeviceId { get; set; }
+        public string Name { get; set; }
+    }
+}
+
---
+++ UpliftLink/Services/JsonService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UpliftLink.Models;
+
+namespace UpliftLink.Services
+{
+    public class JsonService
+    {
+        private readonly string _filePath;
+
+        public JsonService(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public async Task SaveToJsonFileAsync(List<User> users)
+        {
+            var json = JsonConvert.SerializeObject(users, Formatting.Indented);
+            await File.WriteAllTextAsync(_filePath, json);
+        }
+
+        public async Task<List<User>> LoadFromJsonFileAsync()
+        {
+            if (!File.Exists(_filePath))
+                return new List<User>();
+
+            var json = await File.ReadAllTextAsync(_filePath);
+            return JsonConvert.DeserializeObject<List<User>>(json);
+        }
+    }
+}
