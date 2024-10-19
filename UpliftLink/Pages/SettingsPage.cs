311ab399410a4357733265b0c7b1ca8afc622ceb
ej6612 <ej_6612@outlook.com>
2024-10-18 22:22:09 -0600

    Create SettingsPage

---
+++ UpliftLink/Pages/SettingsPage.cs
@@ -0,0 +1,10 @@
+namespace UpliftLink
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
