1e8720cf7724dfc46717447614f4455a1d8f135a
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2024-10-19 02:33:58 -0600

    yo baby what the fetch happened. I swear i did not do all those changes

--- UpliftLink/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/UpliftLink.AssemblyInfo.cs
+++ UpliftLink/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/UpliftLink.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+89bec54dce42fc3b75d7be63ef7dee4a637a15ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7a4b59f1b3715dd43ea8e560eb69cbcdf6b8da1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpliftLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
